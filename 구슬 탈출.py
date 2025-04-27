71f81f35a4e135dcf5090c9ef6c11284034544e5
Factories-git <official.factories@gmail.com>
2025-04-27 13:55:24 +0900

    DFS & BFS

--- 구슬 탈출.py
+++ 구슬 탈출.py
@@ -6,6 +6,18 @@ n, m = map(int, input().split())
 board = [list(input()) for _ in range(n)]
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
+goal = [0] * 2
+blue_bead = [0] * 2
+red_bead = [0] * 2
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 'O':
+            goal[0] = i
+            goal[1] = j
+        if board[i][j] == 'R':
+            red_bead[0], red_bead[1] = i, j
+        if board[i][j] == 'B':
+            blue_bead[0], blue_bead[1] = i, j
 
 
 def bfs(start):
@@ -25,14 +37,15 @@ def bfs(start):
             if board[nx][ny] == '#':
                 continue
             if (d_x != dx[i] or d_y != dy[i]) and board[nx][ny] != '#' and graph[nx][ny] == 0:
-                print('\n'.join(map(str, graph)))
                 queue.append((nx, ny, dx[i], dy[i]))
                 graph[nx][ny] = graph[x][y] + 1
-                print()
                 continue
             if d_x == dx[i] and d_y == dy[i]:
                 graph[nx][ny] = graph[x][y]
                 queue.append((nx, ny, dx[i], dy[i]))
-            print()
-    print('\n'.join(map(str,graph)))
+    print('\n'.join(map(str, graph)))
+    return graph[goal[0]][goal[1]]
 
+
+s = bfs(red_bead + [0, 0])
+print(s)
